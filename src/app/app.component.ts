c64300c9cb36e52a4fbca2f807bd8ecee387c878
Subrat <mastersubrat007@gmail.com>
2020-05-03 21:48:10 +0530

    SwUpdate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ApplicationRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { SwUpdate } from '@angular/service-worker';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'Angular-pwa';
   apiData: any;
-  constructor(private http: HttpClient){}
+  constructor(
+    private http: HttpClient,
+    private update: SwUpdate,
+    private appRef: ApplicationRef
+  ) {
+    this.updateClient();
+    this.checkUpdate();
+  }
 
-  ngOnInit(){
+  ngOnInit() {
     this.http.get('http://dummy.restapiexample.com/api/v1/employees').subscribe(
       (res: any) => {
         this.apiData = res.data;
-      },err => {
+      },
+      (err) => {
         console.error(err);
       }
-    )
+    );
+  }
+
+  updateClient() {
+    if (!this.update.isEnabled) {
+      console.log('Not Enabled');
+      return;
+    }
+    this.update.available.subscribe((event) => {
+      console.log(`current`, event.current, `available `, event.available);
+      if (confirm('update available for the app please conform')) {
+        this.update.activateUpdate().then(() => location.reload());
+      }
+    });
+
+    this.update.activated.subscribe((event) => {
+      console.log(`current`, event.previous, `available `, event.current);
+    });
+  }
+
+  checkUpdate() {
+    this.appRef.isStable.subscribe((isStable) => {
+      if (isStable) {
+        const timeInterval = interval(8 * 60 * 60 * 1000);
+
+        timeInterval.subscribe(() => {
+          this.update.checkForUpdate().then(() => console.log('checked'));
+          console.log('update checked');
+        });
+      }
+    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
